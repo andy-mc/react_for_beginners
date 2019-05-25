1e5c2600e4c317e56ddf3e7ad1692ced64605d5f
Andrés Muñoz <asmz5488@gmail.com>
2019-05-24 20:55:31 -0500

    adding css

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,8 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react';        
 
 class StorePicker extends Component {
+  
   render() { 
-    return <h1>Hello World !!</h1>;
+    return (
+      <form className="store-selector">
+        <h2>Please Enter A Store</h2>
+        
+        <input type="text" required placeholder="Store Name" />
+        <button type="submit">Visit Store ➳</button>
+      </form>
+    );
   }
 }
  
